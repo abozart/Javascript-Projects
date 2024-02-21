4e8f5865ad2814e918bd649051d0bf37338d9be4
abozart <39786888+abozart@users.noreply.github.com>
2024-02-20 19:28:04 -0800

    Escape Character Assignment
    #51

--- Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
@@ -2,3 +2,5 @@ var A = 'This is a string.';
 var B = 'This is an alert.';
 document.write(A);
 window.alert(B);
+
+document.write("<br>Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
