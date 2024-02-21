0fc6f84b44e5a345da57d4f6849c414e5f5af5b9
abozart <39786888+abozart@users.noreply.github.com>
2024-02-20 19:31:03 -0800

    Concatenating
    #52

--- Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
@@ -3,4 +3,7 @@ var B = 'This is an alert.';
 document.write(A);
 window.alert(B);
 
-document.write("<br>Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+document.write("<br><br>Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br><br>");
+document.write("\"Be who you are and say what you feel,"
+                +" because those who mind don't matter and those who matter don't mind.\""
+                +"-Dr Seuss");
