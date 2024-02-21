eae40085c644e04aef008ae2f6d34ceea118501f
abozart <39786888+abozart@users.noreply.github.com>
2024-02-20 19:46:42 -0800

    Basic JS Projects Assignment
    #64

--- Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/js/main.js
@@ -1,16 +1,6 @@
-var A = 'This is a string.';
-var B = 'This is an alert.';
-document.write(A);
-window.alert(B);
+var Sent1 = 'This is the beginning of the string.';
+var Sent2 = ' and this is the end of the string.';
+document.write(Sent1 + Sent2); //Concatenating Sent1 + Sent2
 
-document.write("<br><br>Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br><br>");
-document.write("\"Be who you are and say what you feel,"
-                +" because those who mind don't matter and those who matter don't mind.\""
-                +"-Dr Seuss");
-
-var concString = "Hard eee "+"harrrrr.";
-document.write("<br><br>"+concString+"<br><br>");
-
-var Family = "The Johnsons", Dad = "Joe", Mom = "Sylvia", Daughter = "Penny", Son = "Zorro";
-document.write(Dad+" is: ");
+document.write("<br><br>An Expression: <br>");
 document.write(3 * 18); // basic expression
