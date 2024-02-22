4d61da9c98fc46fb796ae71fb0401c3c3e9ea776
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 14:47:48 -0800

    typeof operator
    #106

---
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,15 @@
+var uObject = {
+    type:'star',
+    type: 7.5, //lightyears across
+    state:'black hole',
+    age:5.3, //billions of years
+    sound:'WHOOOOSH!'
+}
+
+document.write(typeof "fhasgj");
+document.write('<br>');
+document.write(typeof -324.55);
+document.write('<br>');
+document.write(typeof uObject);
+document.write('<br>');
+
