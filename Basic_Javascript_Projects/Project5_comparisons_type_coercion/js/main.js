67b33deab884515441d8e191c694272971f6b76c
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 15:15:19 -0800

    Infinity
    #112

--- Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -2,4 +2,7 @@ function checkNaN(input){
     document.getElementById('nan').innerHTML = isNaN(input);
 }
 
-
+document.write('<br><br>');
+document.write(2E33);
+document.write("<BR>")
+document.write(-2E33233);
