edace5ee260b1d332b79a887d418b74589880645
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 15:13:07 -0800

    NaN tests
    #109

--- Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,15 +1,5 @@
-var uObject = {
-    type:'star',
-    type: 7.5, //lightyears across
-    state:'black hole',
-    age:5.3, //billions of years
-    sound:'WHOOOOSH!'
+function checkNaN(input){
+    document.getElementById('nan').innerHTML = isNaN(input);
 }
 
-document.write(typeof "fhasgj");
-document.write('<br>');
-document.write(typeof -324.55);
-document.write('<br>');
-document.write(typeof uObject);
-document.write('<br>');
 
