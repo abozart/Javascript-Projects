c547c4ba04a9a01dabc70ca3bc1f73261d4e94bd
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 14:42:52 -0800

    Utilizing typeof
    #104

--- Basic_Javascript_Projects/Project4_dictionaries/js/main.js
+++ Basic_Javascript_Projects/Project4_dictionaries/js/main.js
@@ -1,13 +1,21 @@
+var uObject = {
+    type:'star',
+    type: 7.5, //lightyears across
+    state:'black hole',
+    age:5.3, //billions of years
+    sound:'WHOOOOSH!'
+}
+
 function dict(){
-    var uObject = {
-        type:'star',
-        type: 7.5, //lightyears across
-        state:'black hole',
-        age:5.3, //billions of years
-        sound:'WHOOOOSH!'
-    }
+
     delete uObject.sound; //delete object property
     document.getElementById('dictionary').innerHTML = JSON.stringify(uObject); //stringify obj via JSON
 }
 
+document.write(typeof "fhasgj");
+document.write('<br>');
+document.write(typeof -324.55);
+document.write('<br>');
+document.write(typeof uObject);
+document.write('<br>');
 
