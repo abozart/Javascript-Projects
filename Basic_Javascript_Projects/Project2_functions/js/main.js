59f25efb9d59f378eb5f44d4be5b78290f5802aa
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 11:54:01 -0800

    Update

--- Basic_Javascript_Projects/Project2_functions/js/main.js
+++ Basic_Javascript_Projects/Project2_functions/js/main.js
@@ -1,5 +1,5 @@
 function myFunction(){
     var sentence = "I am learning";
     sentence += " a lot from this book!";
-    document.getElementById('conc').innerHTML = sentence;
+    document.getElementById('conc').innerHTML = sentence; //replace conc ID.innerHTML with string
 }
