711da61d88c6e5675c3977b34dd5019976a3ead8
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 13:06:33 -0800

    Remainder
    #86

--- Basic_Javascript_Projects/Project3_math/js/main.js
+++ Basic_Javascript_Projects/Project3_math/js/main.js
@@ -1,6 +1,41 @@
 function add(){
     var num1 = 39;
     var num2 = 324;
-    var addition = num1 + num2;
-    document.getElementById('math').innerHTML = num1+" + "+num2+" = "+ addition;
+    var exp = num1 + num2;
+    document.getElementById('math').innerHTML = num1+" + "+num2+" = "+ exp;
 }
+
+function subtract(){
+    var num1 = 324;
+    var num2 = 39;
+    var exp = num1 - num2;
+    document.getElementById('math2').innerHTML = num1+" - "+num2+" = "+ exp; 
+}
+
+function multiply(){
+    var num1 = 324;
+    var num2 = 39;
+    var exp = num1 * num2;
+    document.getElementById('math3').innerHTML = num1+" x "+num2+" = "+ exp; 
+}
+
+function divide(){
+    console.log('DIVIDE')
+    var num1 = 324;
+    var num2 = 39;
+    var exp = num1 / num2;
+    document.getElementById('math4').innerHTML = num1+" / "+num2+" = "+ exp; 
+}
+
+function allops(){
+    console.log('ALL')
+    var num1 = 324;
+    var num2 = 39;
+    var exp = num1 * num2 + 99 - 384;
+    document.getElementById('math5').innerHTML = num1+" * "+num2+"  + 99 - 384 = "+ exp 
+}
+
+var remainder = 66 % 4
+var math = 64 / 4;
+document.write('<br><br>wooo:')
+document.write('66 / 4 = '+math+' & remainder = '+remainder);
