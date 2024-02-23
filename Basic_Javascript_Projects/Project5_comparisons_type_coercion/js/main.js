b338fa49bf0c305063f2f6bdf197858c7d170c47
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 20:03:22 -0800

    AND / OR / NOT
    #123

--- Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -2,7 +2,6 @@ function checkNaN(input){
     document.getElementById('nan').innerHTML = isNaN(input);
 }
 
-console.log(4 === 4);
-console.log(4 === '4'); // check IF EQUAL and SAME DATA TYPE
-console.log(4 === '433'); // check IF EQUAL and SAME DATA TYPE
-console.log(4 === 24); // check IF EQUAL and SAME DATA TYPE
+console.log(4 == '4' && 2 === 2);
+console.log(4 > 5 || 1 < 5);
+console.log(4 != '433'); 
