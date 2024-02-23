e46cb8726f9dfe6c09cb144edd716fe053eed82d
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 17:09:35 -0800

    Double Equals Assignment
    #119

--- Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -2,6 +2,5 @@ function checkNaN(input){
     document.getElementById('nan').innerHTML = isNaN(input);
 }
 
-
-console.log(1 > 2E33);
-console.log(4 > -2E33233);
+console.log(1 > 2E33); // bool
+console.log(4 == 4); //double equal
