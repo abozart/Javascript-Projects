de08cef1824a192b3965bbd14ac80fc8d8c5d965
abozart <39786888+abozart@users.noreply.github.com>
2024-02-22 20:08:50 -0800

    Project 5 Comparisons TYpe Coercion Assignment
    #126

--- Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,7 +1,5 @@
-function checkNaN(input){
-    document.getElementById('nan').innerHTML = isNaN(input);
-}
-
-console.log(4 == '4' && 2 === 2);
-console.log(4 > 5 || 1 < 5);
-console.log(4 != '433'); 
+document.write(typeof 34); //typeof
+console.log(4 + ' dogs'); //conc number & string
+console.log(4 == '4' && 2 === 2); //if 4==4 AND 2===2
+console.log(4 > 5 || 1 < 5); // bool if 4 is greater than 5 OR  1 is less than 5
+console.log(4 != '433');  //check if 4 DOES NOT equal 433
