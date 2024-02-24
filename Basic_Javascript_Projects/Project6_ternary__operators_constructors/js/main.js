4a8a89864760600c55c84efa7446131346883c14
abozart <39786888+abozart@users.noreply.github.com>
2024-02-23 16:18:44 -0800

    Project 6 Ternary Operators/Constructors Assignment
    #140

--- Basic_Javascript_Projects/Project6_ternary__operators_constructors/js/main.js
+++ Basic_Javascript_Projects/Project6_ternary__operators_constructors/js/main.js
@@ -1,24 +1,12 @@
-/* function Fart(decibels,smell,date,farter) {
-    this.fart_decibels = decibels;
-    this.fart_smell = smell;
-    this.fart_date = date;
-    this.fart_farter = farter;
+function ternary(){
+    console.log('wafawffawfwa')
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var numTest = (num1 > num2) ? 'First # is larger!':'Second # is larger.';
+    document.getElementById('numResult').innerHTML = numTest
 }
 
-var Joe = new Fart('.2','8/10','01/01/1998','Joe B');
-var Clarissa = new Fart('.9','3/10','4/15/2009','Clarissa D.');
-
-var fart_index = [];
-fart_index.push(Joe);
-fart_index.push(Clarissa);
-
-function myFunc(){
-    document.getElementById('keywords_constructors').innerHTML = JSON.stringify(fart_index);
-}
-
-// var typeof = 3323; //assigned reserved keyword to var */
-
-function counter(){
+function counter(){ //nested functions
     document.getElementById('counting').innerHTML = Count();
     function Count(){
         var startingPoint = 9;
@@ -26,4 +14,22 @@ function counter(){
         Plus_one();
         return startingPoint;
     }
+}
+
+function Fart(decibels,smell,date,farter) {
+    this.fart_decibels = decibels;
+    this.fart_smell = smell;
+    this.fart_date = date;
+    this.fart_farter = farter;
+}
+
+var Joe = new Fart('.2','8/10','01/01/1998','Joe B'); // CREATE NEW INSTANCE
+var Clarissa = new Fart('.9','3/10','4/15/2009','Clarissa D.');
+
+var fart_index = []; //add Joe & Clarissa to single Array
+fart_index.push(Joe);
+fart_index.push(Clarissa);
+
+function myObj(){
+    document.getElementById('fahter_result').innerHTML = JSON.stringify(fart_index); //display Fart obj
 }
