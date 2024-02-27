49f14bb16fc867194d44e09bca2ac422c5b75e64
abozart <39786888+abozart@users.noreply.github.com>
2024-02-26 21:07:07 -0800

    Slice Method Assignment
    #159

--- Basic_Javascript_Projects/Project8_string_methods/js/main.js
+++ Basic_Javascript_Projects/Project8_string_methods/js/main.js
@@ -1,3 +1,10 @@
+/*
+    string methods
+.concat
+.slice(0,-3) #remove first and last 3 chars
+
+
+*/
 
 function full_sentence(){
     var part1 = 'I may have ';
@@ -6,4 +13,10 @@ function full_sentence(){
     var part4 = 'sentence.';
     var whole_sentence = part1.concat(part2, part3, part4);
     document.getElementById('output').innerHTML = whole_sentence;
+}
+
+function slice_text(){
+    var sentence = 'All work and no play makes Jack a psychopath!!!';
+    var sliced_section = sentence.slice(3,-3); 
+    document.getElementById('output').innerHTML = sliced_section;
 }
