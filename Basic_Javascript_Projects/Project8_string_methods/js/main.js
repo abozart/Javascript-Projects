b11be8697746bb72b7ef4182e962f49460a109ff
abozart <39786888+abozart@users.noreply.github.com>
2024-02-26 21:23:07 -0800

    Number Method: toString()

--- Basic_Javascript_Projects/Project8_string_methods/js/main.js
+++ Basic_Javascript_Projects/Project8_string_methods/js/main.js
@@ -1,10 +1,9 @@
+
+//  STRING METHODS
+//      .concat(str2, str3, str4...)
+//      .slice(0,-3) #remove first and last 3 chars
+//      .toUpperCase()
 /*
-    string methods
-.concat
-.slice(0,-3) #remove first and last 3 chars
-
-
-*/
 
 function full_sentence(){
     var part1 = 'I may have ';
@@ -19,4 +18,31 @@ function slice_text(){
     var sentence = 'All work and no play makes Jack a psychopath!!!';
     var sliced_section = sentence.slice(3,-3); 
     document.getElementById('output').innerHTML = sliced_section;
+    console.log(sliced_section.toUpperCase()) // UPPPPPPPERCASE
+}
+
+var textStr = "A man wearing a banana hammock does not lay in a hammock at all.";
+var searchFor = 'hamm';
+var searchStr = textStr.search(searchFor);
+
+if(searchStr > -1){
+    console.log('keyword "'+searchFor+'" found at position: '+searchStr)
+}else{
+    console.log('keyword "'+searchFor+'" NOT FOUND in string: \n'+textStr)
+}
+
+*/
+
+//  NUMBER METHODS
+//      .concat(str2, str3, str4...)
+//      .slice(0,-3) #remove first and last 3 chars
+//      .toUpperCase()
+
+function numToStr(){
+    var X = 182;
+    console.log(typeof(X))
+    console.log(X)
+    var Y = X.toString()
+    console.log(typeof(Y))
+    console.log(Y)
 }
