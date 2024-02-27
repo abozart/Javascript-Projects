8098077dc55f40db6fdd4d52dbe40167abe0e784
abozart <39786888+abozart@users.noreply.github.com>
2024-02-26 17:59:10 -0800

    If/Else & Date().getHours()
    #146

--- Basic_Javascript_Projects/Project7_scope_time_function/js/main.js
+++ Basic_Javascript_Projects/Project7_scope_time_function/js/main.js
@@ -1,3 +1,5 @@
+/* SCOPE CHALLENGE
+
 var global_var = 5;
 
 function myFunc(){
@@ -8,3 +10,23 @@ function myFunc(){
 myFunc();
 
 console.log(local_var) // inaccessible because its in a function
+
+END SCOPE CHALLENGE */
+
+// IF STATEMENT
+if (4 != 10){
+    console.log('numbers are not equal')
+}
+// END IF STATEMENT
+
+// DATE
+function get_hours(hourCompare){
+    var currentHour = new Date().getHours();
+    if(currentHour < hourCompare){
+        var msg = 'It\'s before '+hourCompare+'00.';
+    }else{
+        var msg = 'It\'s past '+hourCompare+'00'
+    }
+    document.getElementById('greeting').innerHTML = msg+' Time = '+currentHour+' : Compare time = '+hourCompare;
+}
+// END DATE
