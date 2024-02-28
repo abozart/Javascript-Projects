4b6d5fab402c4e2103a2965b07130538f98a9067
abozart <39786888+abozart@users.noreply.github.com>
2024-02-27 18:59:16 -0800

    Project9: Countdown/Slideshow
    #172

--- Basic_Javascript_Projects/Project9_countdown_slideshow/js/main.js
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/js/main.js
@@ -7,9 +7,40 @@ function countdown(){
         var time = setTimeout(tick, 1000);
         if (seconds == -1){
             clearTimeout(time);
-            timer.innerHTML = "";
+            timer.innerHTML = "BEEP BEEP!@#@$ WAKE UP DUDE!#@#";
         }
     
     }
     tick();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
+    if(n > slides.length) {slideIndex = 1 }
+    if(n < 1) {slideIndex = slides.length}
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(' active',''); //replace active class
+    }
+    console.log(slides)
+    console.log(slideIndex-1)
+    console.log(dots)
+
+    slides[slideIndex-1].style.display = 'block'; //show particlar slide
+    dots[slideIndex-1].className += ' active';
 }
