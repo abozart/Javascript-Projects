897bdf7f605bdd8ab49008172f91c3ff60b0e072
abozart <39786888+abozart@users.noreply.github.com>
2024-02-29 17:48:11 -0800

    Updated slideshow

--- Basic_Javascript_Projects/Project9_countdown_slideshow/js/main.js
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/js/main.js
@@ -37,10 +37,45 @@ function showSlides(n){
     for(i = 0; i < dots.length; i++){
         dots[i].className = dots[i].className.replace(' active',''); //replace active class
     }
-    console.log(slides)
-    console.log(slideIndex-1)
-    console.log(dots)
 
     slides[slideIndex-1].style.display = 'block'; //show particlar slide
     dots[slideIndex-1].className += ' active';
+}
+
+
+document.onkeydown = checkKey;
+/* function checkKey(e) {
+
+    switch(e.keyCode) {
+        case 38:
+            console.log('up')
+            break;
+        case 40:
+            console.log('down')
+            break;
+        case 37:
+            console.log('left')
+            plusSlides(-1)
+            break;
+        case 39:
+            console.log('right')
+            plusSlides(1)
+            break;
+        default:
+          // code block
+          console.log('You pressed key: '+e.keyCode)
+      } 
+
+} */
+
+function checkKey(e) {
+    switch(e.keyCode) {
+        case 37: //left arrow
+            plusSlides(-1)
+            break;
+        case 39: //right arrow
+            plusSlides(1)
+            break;
+      } 
+
 }
