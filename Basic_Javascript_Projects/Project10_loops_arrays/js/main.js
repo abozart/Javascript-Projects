df6cb05581d07c691c8286b1a54c75ec90525539
abozart <39786888+abozart@users.noreply.github.com>
2024-03-01 15:33:00 -0800

    Project 10: const Assignment
    #213

---
+++ Basic_Javascript_Projects/Project10_loops_arrays/js/main.js
@@ -0,0 +1,40 @@
+//.length property
+var myStr = 'supercalifragilistic expialadocius';
+document.getElementById('output').innerHTML = "String = "+myStr+"<br>string length: "+myStr.length;
+
+// loop example
+var instruments = ['Guitar','Piano','Woodwinds','Drums','Brass','Horns'];
+var content = '';
+var Y;
+function for_loop(){
+    for(Y = 0; Y < instruments.length; Y++){
+        content += instruments[Y]+'<br>';
+    }
+    document.getElementById('instrument_list').innerHTML = content;
+}
+
+// array example
+function array_func(){
+    var myArray = ['doh','re','me','fa','so','la','ti','doh'];
+    document.getElementById('array').innerHTML = random_item(myArray);
+}
+
+function random_item(items) {
+    // Use Math.random() to generate a random number between 0 and 1,
+    // multiply it by the length of the array, and use Math.floor() to round down to the nearest integer
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+//constant example
+function constant_func(){
+    //const dude = "Hey Dude!";
+    // dude = 'asfgasg'; //display error
+
+    const dudeObj = {where:'ismycar',a:'wooooah',b:'YO'}
+    console.log(dudeObj);
+    dudeObj.b = 'YO YO';
+    dudeObj.c = 1242;
+    console.log(dudeObj)
+
+    document.getElementById('constant').innerHTML = JSON.stringify(dudeObj);
+}
