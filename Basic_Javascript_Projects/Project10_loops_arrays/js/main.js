da38fec21a06515a98b60dd38396b3d0c4148272
abozart <39786888+abozart@users.noreply.github.com>
2024-03-01 18:33:22 -0800

    Project 10: Break/Continue Statement
    #222

--- Basic_Javascript_Projects/Project10_loops_arrays/js/main.js
+++ Basic_Javascript_Projects/Project10_loops_arrays/js/main.js
@@ -25,26 +25,63 @@ function constant_func(){
     // dude = 'asfgasg'; //display error
 
     const dudeObj = {where:'ismycar',a:'wooooah',b:'YO'}
-    console.log(dudeObj);
+    //.log(dudeObj);
     dudeObj.b = 'YO YO';
     dudeObj.c = 1242;
-    console.log(dudeObj)
+    //console.log(dudeObj)
 
     document.getElementById('constant').innerHTML = JSON.stringify(dudeObj);
 }
 
-//let, block scope
-var X = 325;
-console.log(X)
-if(1==1){
-    let X = 75;
+//let example of block scope
+function let_func(){
+    var X = 325;
     console.log(X)
+    if(1==1){
+        let X = 75;
+        console.log(X)
+    }
+    console.log(X);
 }
-console.log(X);
 
 // return statement
 function random_item(items) {
     // Use Math.random() to generate a random number between 0 and 1,
     // multiply it by the length of the array, and use Math.floor() to round down to the nearest integer
     return items[Math.floor(Math.random() * items.length)];
+}
+
+//object assignment
+function object_method(){
+    let product = {
+        color: 'red',
+        size: 'bigger than a UAP, smaller than a submarine',
+        weight: 200, //lbs
+        unitCost: 1.74999, //$USD per lb
+        totalCost: function(){
+            return this.unitCost * this.weight
+        }
+    };
+    document.getElementById('object_method').innerHTML = product.totalCost();
+}
+
+
+function break_func(){
+    var text = new String;
+    for (let i = 0; i < 10; i++) {
+        if (i === 3) { break; }
+        text += "The number is " + i + "<br>";
+      }
+      console.log(text);
+      document.getElementById('break_continue').innerHTML = text;
+      
+}
+function continue_func(){
+    var text = new String;
+    for (let i = 0; i < 10; i++) {
+        if (i === 3) { continue; }
+        text += "The number is " + i + "<br>";
+      }
+      console.log(text);
+      document.getElementById('break_continue').innerHTML = text;
 }
