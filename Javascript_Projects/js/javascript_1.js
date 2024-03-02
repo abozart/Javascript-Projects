3ddb998094fca390c90fa75575123e009f0d667c
abozart <39786888+abozart@users.noreply.github.com>
2024-03-01 21:40:02 -0800

    Switch Statement
    Javascript_Projects #225

---
+++ Javascript_Projects/js/javascript_1.js
@@ -0,0 +1,33 @@
+function color_func(){
+    var colorOutput;
+    var color = document.getElementById('color_input').value;
+    console.log(color)
+    var colorString = ' is a great color!';
+    switch(color.toLowerCase()){
+        case 'red':
+            color_output = 'red' + colorString;
+        break;
+        case 'yellow':
+            color_output = 'yellow' + colorString;
+        break;
+        case 'green':
+            color_output = 'green' + colorString;
+        break;
+        case 'blue':
+            color_output = 'blue' + colorString;
+        break;
+        case 'purple':
+            color_output = 'purple' + colorString;
+        break;
+        case 'pink':
+            color_output = 'pink' + colorString;
+        break;
+        default:
+            color_output = color+ ' is not on a color on the list.'
+    }
+
+    x = document.getElementById('switch_output');
+    x.style.color = color;
+    x.style.fontWeight = "bold";
+    document.getElementById('switch_output').innerHTML = color_output;
+}
