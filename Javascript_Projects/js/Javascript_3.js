86ef3752d17bafd108515cc8724e479c547d04e0
abozart <39786888+abozart@users.noreply.github.com>
2024-03-08 15:31:29 -0800

    Update Javascript_3.js

--- Javascript_Projects/js/Javascript_3.js
+++ Javascript_Projects/js/Javascript_3.js
@@ -2,3 +2,15 @@ function displayMood(moodObj){
     var mood = moodObj.getAttribute('data-mood');
     alert(mood);
 }
+
+// SYNTAX
+// document.querySelector('p').style.backgroundColor = 'blue'; //change first p element to bg-color to blue
+// document.querySelectorAll("p") // return list of p elements
+
+ // console.log(parseFloat('32a434aadewfa')); //return integer from string (or first series of integers within a alphanumeric string)
+ //                                             otherwise it returns NaN
+
+ // TERMINOLOGY
+ // (!var) -> the !exclamation is called a "bang"
+
+ 
