9da8a1e96ff98c9c26d13705ce19982929497861
abozart <39786888+abozart@users.noreply.github.com>
2024-03-18 17:42:13 -0700

    basic jQuery

--- Javascript_Projects/jQuery/js.js
+++ Javascript_Projects/jQuery/js.js
@@ -2,6 +2,9 @@ const h1 = document.createElement('h1');
 h1.innerHTML = "Hello World!";
 document.body.appendChild(h1);
 
-console.log(JSON.stringify($("html")[0].outerHTML))
+console.log(JSON.stringify($("html")[0].outerHTML)) //get stringified version of html
 
-$('h1').hide(1000)
+
+$('#dada').on('click', function(){ //when #dada is clicked hide h1
+    $('h1').hide(1000)
+});    
